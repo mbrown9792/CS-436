c887a9743ab3df01877c16cf08e13fd6a97c7265
Michael Brown <mbrown9792@gmail.com>
2015-04-20 12:31:01 -0500

    Final Update

--- cs_436_demo.c
+++ cs_436_demo.c
@@ -4,17 +4,19 @@
 #include<string.h>
 
 void func(char *str) {
-	char buffer[24];
+	char buffer[24];	
 	int *ret;
+	ret = buffer + 40;
+	(*ret) += 17;
  	strcpy(buffer,str);
 }
-
+	
 int main(int argc, char **argv) {
 	int x;
 	x = 0;
 	func(argv[1]);
 	x = 1;
- 	printf("x is 1”);
- 	printf("x is 0”);
+ 	printf("x is 1\n");
+ 	printf("x is 0\n");
 }
 //END
